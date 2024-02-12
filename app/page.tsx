6d19b592720209577523523fdc4dafe124d112c4
Nathaniel <131909495+nathanielrz@users.noreply.github.com>
2024-02-12 12:36:56 -0800

    add more projects

--- app/page.tsx
+++ app/page.tsx
@@ -220,7 +220,7 @@ export default function Home() {
           </a>
         </div>
         <p className="mt-6 text-sm">Some of my projects: </p>
-        <div className="flex items-center justify-center gap-2">
+        <div className="grid grid-rows-2 grid-cols-3 items-center justify-center gap-2">
           <a href="https://github.com/nathanielrz/console-os" target="_blank">
             <div className="bg-zinc-900 rounded-xl p-4 mt-2 inline-block w-80 hover:scale-95 hover:bg-zinc-800 transition ease-in-out duration-150">
               <p className="font-bold text-lg mb-2">Console OS</p>
@@ -244,6 +244,30 @@ export default function Home() {
               <p className="text-sm text-zinc-400 w-60 inline-block text-left">
                 A free-to-play online game website available to everyone in
                 every countr...
+              </p>
+            </div>
+          </a>
+          <a
+            href="https://github.com/nathanielrz/calculator-in-react"
+            target="_blank"
+          >
+            <div className="bg-zinc-900 rounded-xl p-4 mt-2 inline-block w-80 hover:scale-95 hover:bg-zinc-800 transition ease-in-out duration-150">
+              <p className="font-bold text-lg mb-2">Calculator in React</p>
+              <p className="text-sm text-zinc-400 w-60 inline-block text-left">
+                A remake of apple's calculator in react - contributions are
+                appreciated
+              </p>
+            </div>
+          </a>
+          <a
+            href="https://github.com/nathanielrz/calculator-in-react"
+            target="_blank"
+          >
+            <div className="bg-zinc-900 rounded-xl p-4 mt-2 inline-block w-80 hover:scale-95 hover:bg-zinc-800 transition ease-in-out duration-150">
+              <p className="font-bold text-lg mb-2">Discord Profile Status</p>
+              <p className="text-sm text-zinc-400 w-60 inline-block text-left">
+                A react package for using Discord's profile picture and statuses
+                in your websites
               </p>
             </div>
           </a>
