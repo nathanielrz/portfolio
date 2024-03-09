d4abdea5bf69b7a02df94759743e61932823ae98
Nathaniel <nathaniel_ramirez@icloud.com>
2024-03-09 10:18:10 -0800

    fix bugt

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,24 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const [backdrop, setBackdrop] = useState(false);
-  window.onscroll = function () {
-    if (scrollY > 1) {
-      setBackdrop(true);
-    } else {
-      setBackdrop(false);
-    }
-  };
+  useEffect(() => {
+    window.onscroll = function () {
+      if (scrollY > 1) {
+        setBackdrop(true);
+      } else {
+        setBackdrop(false);
+      }
+    };
+  }, []);
   return (
     <nav
       className={`fixed top-0 left-0 w-screen h-16 ${
-        backdrop ? "backdrop-blur-2xl bg-[rgba(72,107,142,0.2)] drop-shadow-2xl" : ""
+        backdrop
+          ? "backdrop-blur-2xl bg-[rgba(72,107,142,0.2)] drop-shadow-2xl"
+          : ""
       } text-center text-white text-base z-10 flex justify-center items-center transition-all ease-in-out delay-0 duration-300`}
     >
       <div className="flex justify-between items-center w-[90%]">
